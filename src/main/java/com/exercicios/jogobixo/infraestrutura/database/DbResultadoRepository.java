f97c1eede7b817eafa2e989c92d0d6b3b2bfe149
lucasokada <lucas.okada@unesp.br>
2023-03-14 13:09:06 -0300

    feat: muda a fonte de dados e como os dados sao importados

--- src/main/java/com/exercicios/jogobixo/infraestrutura/database/DbResultadoRepository.java
+++ src/main/java/com/exercicios/jogobixo/infraestrutura/database/DbResultadoRepository.java
@@ -4,10 +4,12 @@ import com.exercicios.jogobixo.core.dominio.ResultadoDia;
 import com.exercicios.jogobixo.core.dominio.ResultadoRepository;
 import com.exercicios.jogobixo.infraestrutura.database.entities.ResultadoDiaEntity;
 import com.exercicios.jogobixo.infraestrutura.database.entities.ResultadoDiaJpaRepository;
+import com.exercicios.jogobixo.infraestrutura.database.entities.ResultadoHorarioEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Optional;
 
 @Component
@@ -18,6 +20,13 @@ public class DbResultadoRepository implements ResultadoRepository {
     @Override
     public Optional<ResultadoDia> consultarPorData(LocalDate filtro) {
         var resultadoDia = this.resultadoDiaJpaRepository.findById(filtro);
+        resultadoDia.get().getHorarios().sort(new Comparator<ResultadoHorarioEntity>() {
+            @Override
+            public int compare(ResultadoHorarioEntity o1, ResultadoHorarioEntity o2) {
+                return o1.getOrdem() - o2.getOrdem();
+            }
+        });
+
         return resultadoDia.map(ResultadoDiaEntity::toDomain);
     }
 
